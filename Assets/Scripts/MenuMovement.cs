295f042e553df28389fe366ec90e962dfc15f366
Daniel Clarke <danclarke88@gmail.com>
2017-11-30 14:51:21 -0500

    Logic to move the menus around.
    TODO: animate it

---
+++ Assets/Scripts/MenuMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMovement : MonoBehaviour {
+    //TODO: Make this smarter?
+    public float speed = 3;
+
+    List<RectTransform> menus = new List<RectTransform>();
+
+    private void Start()
+    {
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            menus.Add(transform.GetChild(i).GetComponent<RectTransform>());
+        }
+    }
+
+    public void MoveLeft()
+    {
+        foreach(RectTransform menu in menus)
+        {
+            menu.position = new Vector2(menu.transform.position.x - 6.6f, 0);
+        }
+    }
+
+    public void MoveRight()
+    {
+        foreach (RectTransform menu in menus)
+        {
+            menu.position = new Vector2(menu.transform.position.x + 6.6f, 0);
+        }
+    }
+}
