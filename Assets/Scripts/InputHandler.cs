59b000c1e64d6fc1c01bca74acf0ebe620f35b46
Liam Collins <liamcollins333@gmail.com>
2017-12-08 11:31:00 -0500

    InputHandler abstracts all player input and categorizes it based on InputType. Utility has some utility functions (obviously) and the InputType enum.

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utility;
+
+public class InputHandler {
+
+    private MultiValueDictionary<InputType, Delegate> _inputMap;
+    /// <summary>
+    /// Returns a clone of the private "_inputMap".
+    /// </summary>
+    public MultiValueDictionary<InputType, Delegate> InputMap
+    {
+        get
+        {
+            return new MultiValueDictionary<InputType, Delegate>(_inputMap);
+        }
+    }
+
+    public InputHandler()
+    {
+        //Debug.Log("I am an InputHandler");
+        RestoreDefaults();
+    }
+
+	private void RestoreDefaults () {
+        //Default Control Scheme
+        _inputMap = new MultiValueDictionary<InputType, Delegate> ();
+        Func<bool> helloWorld = () => Input.GetKeyDown(KeyCode.H);
+        _inputMap.Add(InputType.HelloWorld, helloWorld);
+        Func<bool> left = () => Input.GetKey(KeyCode.A);
+        _inputMap.Add(InputType.Left, left);
+        Func<bool> right = () => Input.GetKey(KeyCode.D);
+        _inputMap.Add(InputType.Right, right);
+        Func<bool> up = () => Input.GetKey(KeyCode.W);
+        _inputMap.Add(InputType.Up, up);
+        Func<bool> down = () => Input.GetKey(KeyCode.S);
+        _inputMap.Add(InputType.Down, down);
+
+
+    }
+
+    // High-Level Input Functions
+    /// <summary>
+    /// Checks all input delegates for a given InputType.
+    /// Returns true if ANY input delegate returns true.
+    /// </summary>
+    /// <param name="inputType">The InputType to check.</param>
+    /// <returns>bool: The *OR* output of all input delegates.</returns>
+    public bool GetPlayerInputBool(InputType inputType)
+    {
+        List<Delegate> delegates = _inputMap[inputType];
+        foreach(Delegate del in delegates)
+        {
+            bool value = GetPlayerInputBool(del);
+            if (value)
+                return true;
+        }
+        return false;
+    }
+    /// <summary>
+    /// Checks all input delegates for a given InputType.
+    /// Returns the maximum value returned by the delegates.
+    /// </summary>
+    /// <param name="inputType"></param>
+    /// <returns>The max output of all input delegates.</returns>
+    public float GetPlayerInputFloat(InputType inputType)
+    {
+        List<Delegate> delegates = _inputMap[inputType];
+        float maxValue = 0;
+        foreach (Delegate del in delegates)
+        {
+            float value = GetPlayerInputFloat(del);
+            if (value > 1)
+            {
+                Debug.LogError("GetPlayerInputFloat: Input delegate " + del + " returned > 1; setting to 1.");
+                value = 1;
+            }
+            else if (value < 0)
+            {
+                Debug.LogError("GetPlayerInputFloat: Input delegate " + del + " returned < 0; setting to 0.");
+                value = 0;
+            }
+            if (value > maxValue)
+                maxValue = value;
+        }
+        return maxValue;
+    }
+
+    // Low-Level Input Functions
+
+    /// <summary>
+    /// Checks a player input delegate.
+    /// </summary>
+    /// <param name="inputFunction">The input delegate to check.</param>
+    /// <returns>bool :: The output of the input delegate, as a bool.</returns>
+    bool GetPlayerInputBool(Delegate inputFunction)
+    {
+        var value = inputFunction.DynamicInvoke();
+        // Validation
+        if      (value.GetType() == typeof(bool)) return (bool) value;
+        else if (value.GetType() == typeof(float)) return TypeCast.FloatToBool((float) value);
+        else    Debug.LogError("GetPlayerInputBool: Unexpected return type from input delegate " + inputFunction + "; returning false.");
+        return false;
+    }
+    /// <summary>
+    /// Checks a player input delegate.
+    /// </summary>
+    /// <param name="inputFunction">The input delegate to check.</param>
+    /// <returns>float: The output of the input delegate, as a float.</returns>
+    float GetPlayerInputFloat(Delegate inputFunction)
+    {
+        var value = inputFunction.DynamicInvoke();
+        // Validation
+        if (value.GetType() == typeof(float)) return (float) value;
+        else if (value.GetType() == typeof(bool)) return TypeCast.BoolToFloat((bool) value);
+        else Debug.LogError("GetPlayerInputFloat: Unexpected return type from input delegate" + inputFunction + "; returning 0.");
+        return 0;
+    }
+}
---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utility
+{
+    public enum InputType { Left, Right, Jump, Up, Down, Pause, UseItem, HelloWorld };
+
+    public static class TypeCast
+    {
+        public static float BoolToFloat(bool value) { return (float)((value) ? 1 : 0); }
+        public static int BoolToInt(bool value) { return (int)((value) ? 1 : 0); }
+
+        public static bool FloatToBool(float value) { return (bool)(value != 0); }
+        public static bool IntToBool(int value) { return (bool)(value != 0); }
+    }
+
+    public class MultiValueDictionary<K, V> : Dictionary<K, List<V>>
+    {
+
+        public MultiValueDictionary() { }
+
+        /// <summary>
+        /// Becomes a clone of a given MVD.
+        /// </summary>
+        /// <param name="original">The MVD to clone.</param>
+        public MultiValueDictionary(MultiValueDictionary<K, V> original)
+        {
+            foreach (KeyValuePair<K,List<V>> kvp in original)
+            {
+                Add(kvp.Key,kvp.Value);
+            }
+        }
+
+        public void Add(K TKey, V TValue)
+        {
+            // If this is not the first value for the key
+            if (ContainsKey(TKey))
+            {
+                this[TKey].Add(TValue);
+            }
+            // If this is the first value for the key
+            else
+            {
+                List<V> values = new List<V>();
+                values.Add(TValue);
+                Add(TKey, values);
+            }
+        }
+
+        public void Remove(K TKey, V TValue)
+        {
+            if (ContainsKey(TKey))
+            {
+                List<V> values = this[TKey];
+                if (values.Contains(TValue))
+                {
+                    if (values.Count == 1)
+                        Remove(TKey);
+                    else
+                        this[TKey].Remove(TValue);
+
+                }
+                else
+                {
+                    Debug.LogError("Tried to remove value " + TValue + " from key " + TKey + "; no such value exists.");
+                }
+            }
+            else
+            {
+                Debug.LogError("Tried to remove value " + TValue + " from key " + TKey + "; no such key exists.");
+            }
+        }
+
+    }
+}
