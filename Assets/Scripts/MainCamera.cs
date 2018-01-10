88298800f865357988ea9d04a875d14553add5dc
Jonathan Stanford <1js36@queensu.ca>
2018-01-09 20:45:38 -0500

    Added in an empty saw object, and coded in the functionality to make the camera follow it instead of the player if the saw gets too low

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -5,27 +5,44 @@ using UnityEngine;
 public class MainCamera : MonoBehaviour {
 
     private Transform _playerTransform;
+    private Transform _sawTransform;
 
 	// Use this for initialization
 	void Start () {
         _playerTransform = GameObject.Find("Player").transform;
-	}
+        _sawTransform = GameObject.Find("Saw").transform;
+    }
 
     private int _cameraLeashLength = 20; //number is temporary, will be replaced with calculation based off of "world height"
+    private int _sawDistance = 35; //number is temporary, will be replaced with calculation based off of "world height" (should be half-ish world height)
+    private bool sawTakeOver = false; //if camera starts following the saw, the camera jitters without this when the player is too close to the top of the screen
+    private int sawSize = 5; //the amount of the saw image we want to show up on the screen before the camera starts moving
 
     // Update is called once per frame
     void Update () {
         Vector2 playerPosition = _playerTransform.position;
+        Vector2 sawPosition = _sawTransform.position;
         Vector2 cameraPosition = transform.position;
         
-        if (playerPosition.y - cameraPosition.y < - _cameraLeashLength) 
+        if (sawPosition.y + sawSize - cameraPosition.y < _sawDistance) //takes priority over player following
         {
-            cameraPosition.y = playerPosition.y + _cameraLeashLength;
+            cameraPosition.y = sawPosition.y + sawSize - _sawDistance;
+            sawTakeOver = true;
         }
-        else if (playerPosition.y - cameraPosition.y > _cameraLeashLength)
+        else
         {
-            cameraPosition.y = playerPosition.y - _cameraLeashLength;
+            if (playerPosition.y - cameraPosition.y < -_cameraLeashLength)
+            {
+                cameraPosition.y = playerPosition.y + _cameraLeashLength;
+                sawTakeOver = false; //starts following player when they outrun the saw enough
+            }
+            else if ((playerPosition.y - cameraPosition.y > _cameraLeashLength) && !sawTakeOver) //extra check prevents camera jitter
+            {
+                cameraPosition.y = playerPosition.y - _cameraLeashLength;
+            }
         }
         transform.position = new Vector3(cameraPosition.x, cameraPosition.y, -10);
+        //to be removed eventually:
+        if (sawPosition.y < playerPosition.y) print("dead");
     }
 }
