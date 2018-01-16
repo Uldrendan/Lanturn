0d29a105eeb086b035ed1bc0bdaafc00f7501763
Jonathan Stanford <1js36@queensu.ca>
2018-01-15 20:29:21 -0500

    added in smooth camera movements, a temporary saw sprite, and a super simple saw movement script

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -16,7 +16,8 @@ public class MainCamera : MonoBehaviour {
     private int _cameraLeashLength = 20; //number is temporary, will be replaced with calculation based off of "world height"
     private int _sawDistance = 35; //number is temporary, will be replaced with calculation based off of "world height" (should be half-ish world height)
     private bool sawTakeOver = false; //if camera starts following the saw, the camera jitters without this when the player is too close to the top of the screen
-    private int sawSize = 5; //the amount of the saw image we want to show up on the screen before the camera starts moving
+    private int sawSize = 3; //the amount of the saw image we want to show up on the screen before the camera starts moving
+    private float _cameraSmoothing = 2; //higher values = snappier camera movements
 
     // Update is called once per frame
     void Update () {
@@ -24,24 +25,25 @@ public class MainCamera : MonoBehaviour {
         Vector2 sawPosition = _sawTransform.position;
         Vector2 cameraPosition = transform.position;
         
-        if (sawPosition.y + sawSize - cameraPosition.y < _sawDistance) //takes priority over player following
+        
+        
+        if (playerPosition.y - cameraPosition.y < -_cameraLeashLength) // player approaches bottom of screen
+        {
+            cameraPosition.y = playerPosition.y + _cameraLeashLength - 1; // the negative value is to deal with lerp never "quite" reaching the target location 
+            sawTakeOver = false; //starts following player when they outrun the saw enough
+            transform.position = Vector3.Lerp(transform.position, new Vector3(cameraPosition.x, cameraPosition.y, -10), Time.deltaTime * _cameraSmoothing);
+        }
+        else if ((playerPosition.y - cameraPosition.y > _cameraLeashLength) && !sawTakeOver) // player approaches top of screen (extra check prevents camera jitter)
+        {
+            cameraPosition.y = playerPosition.y - _cameraLeashLength;
+            transform.position = Vector3.Lerp(transform.position, new Vector3(cameraPosition.x, cameraPosition.y, -10), Time.deltaTime * _cameraSmoothing);
+        }
+        else if (sawPosition.y + sawSize - cameraPosition.y < _sawDistance) //saw reaches edge of screen
         {
             cameraPosition.y = sawPosition.y + sawSize - _sawDistance;
             sawTakeOver = true;
+            transform.position = new Vector3(cameraPosition.x, cameraPosition.y, -10);
         }
-        else
-        {
-            if (playerPosition.y - cameraPosition.y < -_cameraLeashLength)
-            {
-                cameraPosition.y = playerPosition.y + _cameraLeashLength;
-                sawTakeOver = false; //starts following player when they outrun the saw enough
-            }
-            else if ((playerPosition.y - cameraPosition.y > _cameraLeashLength) && !sawTakeOver) //extra check prevents camera jitter
-            {
-                cameraPosition.y = playerPosition.y - _cameraLeashLength;
-            }
-        }
-        transform.position = new Vector3(cameraPosition.x, cameraPosition.y, -10);
         //to be removed eventually:
         if (sawPosition.y < playerPosition.y) print("dead");
     }
---
+++ Assets/Scripts/SawScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    private float _sawSpeed = 2;
+
+	// Update is called once per frame
+	void Update () {
+        Vector2 newPosition = transform.position;
+        newPosition.y -= Time.deltaTime * _sawSpeed;
+        transform.position = new Vector3(newPosition.x, newPosition.y, 0);
+	}
+}
